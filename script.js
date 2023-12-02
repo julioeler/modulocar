a9c8b96116c18aaa0e4fbb506ffade26b497fb7c
julioeler <julioeler221@gmail.com>
2023-12-02 18:31:39 -0300

    Primeiro commit: HTML, CSS e JS do header pronto

---
+++ script.js
@@ -0,0 +1,15 @@
+
+
+const MENULISTA = document.querySelector('.jsHeaderMenu');
+const MENU = document.querySelector('.jsMenuHamburguer');
+const MENUICONE = document.querySelector('.jsMenuIcon');
+
+function ativarMenu(){
+  MENULISTA.classList.toggle('ativo');
+  MENUICONE.classList.toggle('ativo');
+}
+
+MENU.addEventListener('click', ativarMenu);
+
+
+
