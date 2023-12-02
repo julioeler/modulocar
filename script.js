cff8152a852f4a54e57384894de86895dd491690
julioeler <julioeler221@gmail.com>
2023-12-02 18:35:21 -0300

    Criado README"

--- script.js
+++ script.js
@@ -1,15 +1,16 @@
 
 
+
 const MENULISTA = document.querySelector('.jsHeaderMenu');
 const MENU = document.querySelector('.jsMenuHamburguer');
 const MENUICONE = document.querySelector('.jsMenuIcon');
 
+if(MENU) {
 function ativarMenu(){
   MENULISTA.classList.toggle('ativo');
   MENUICONE.classList.toggle('ativo');
 }
-
 MENU.addEventListener('click', ativarMenu);
-
+}
 
 
