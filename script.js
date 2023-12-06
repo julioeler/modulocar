056597da3b1c9e168b8694d832c5aea74e20d6ed
julioeler <julioeler221@gmail.com>
2023-12-06 17:15:45 -0300

    feat: criado article de servicos no index -HTML do servicos -CSS do servicos -JS do servicos update: alterado alguns arquivos gerais -Alterado componentes.css -Adicionado uma imagem dos servicos e alterado alguns arquivos gerais de estilo

--- script.js
+++ script.js
@@ -1,15 +1,40 @@
-
 // JS HEADER
 const MENULISTA = document.querySelector('.jsHeaderMenu');
 const MENU = document.querySelector('.jsMenuHamburguer');
 const MENUICONE = document.querySelector('.jsMenuIcon');
 
-if(MENU) {
-function ativarMenu(){
-  MENULISTA.classList.toggle('ativo');
-  MENUICONE.classList.toggle('ativo');
-}
-MENU.addEventListener('click', ativarMenu);
+if (MENU) {
+  function ativarMenu() {
+    MENULISTA.classList.toggle('ativo');
+    MENUICONE.classList.toggle('ativo');
+  }
+  MENU.addEventListener('click', ativarMenu);
 }
 
 //-----------------------------------------------------
+
+// JS SERVIÇOS
+const SERVICOCONTEUDO = document.querySelectorAll('.jsServicoConteudo');
+const SERVICOTITULO = document.querySelectorAll('.jsServicoTitulo');
+
+
+if (SERVICOCONTEUDO && SERVICOTITULO) {
+  function ativarItem(index) {
+    SERVICOTITULO.forEach((element) => {
+      element.classList.remove('ativo');
+    })
+
+    SERVICOCONTEUDO.forEach((element) => {
+      element.classList.remove('ativo');
+    })
+
+    SERVICOTITULO[index].classList.add('ativo');
+    SERVICOCONTEUDO[index].classList.add('ativo');
+  }
+
+  SERVICOTITULO.forEach((item, index) => {
+    item.addEventListener('click', () => {
+      ativarItem(index)
+    })
+  });
+}
