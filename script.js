58068d8d65666f0493c10945e323f910713bc0e1
julioeler <julioeler221@gmail.com>
2023-12-09 01:53:26 -0300

    feat criado painel -Criado HTML, CSS e JS do painel e Layout Responsivo update: alterado arquivos css e icones -Modificado arquivos css para pequenas correções e adicionado simbolos do painel nos arquivos imagens/simboloPainel"

--- script.js
+++ script.js
@@ -17,7 +17,6 @@ if (MENU) {
 const SERVICOCONTEUDO = document.querySelectorAll('.jsServicoConteudo');
 const SERVICOTITULO = document.querySelectorAll('.jsServicoTitulo');
 
-
 if (SERVICOCONTEUDO && SERVICOTITULO) {
   function ativarItem(index) {
     SERVICOTITULO.forEach((element) => {
@@ -38,3 +37,49 @@ if (SERVICOCONTEUDO && SERVICOTITULO) {
     })
   });
 }
+
+
+//-----------------------------------------------------
+
+// JS PAINEL
+
+const LISTASIMBOLOS = document.querySelectorAll('.jsListaSimbolos li');
+const SIMBOLO = document.querySelectorAll('.jsListaSimbolos li svg');
+const TEXTOS = document.querySelectorAll('.jsListaTextoSimbolo li p');
+
+if (LISTASIMBOLOS) {
+  function ativarSimbolo(index) {
+    SIMBOLO.forEach((simbolo) => {
+      simbolo.classList.remove('ativo');
+    });
+    TEXTOS.forEach((texto) => {
+      texto.classList.remove('ativo');
+    });
+
+    SIMBOLO[index].classList.add('ativo');
+    TEXTOS[index].classList.add('ativo');
+
+  }
+
+
+  LISTASIMBOLOS.forEach((simbolo, index) => {
+    simbolo.addEventListener('click', () => {
+      ativarSimbolo(index);
+    })
+  })
+}
+
+// TROCAR A IMAGEM PAINEL
+const IMAGEMPAINEL = document.querySelector('.painelImagem img');
+
+// Função para trocar a imagem quando a largura da tela for maior ou igual a 600px
+function trocarImagem() {
+  if (window.innerWidth <= 600) {
+    IMAGEMPAINEL.src = "./imagens/outros/painelMobile.jpg";
+  } else {
+    IMAGEMPAINEL.src = "./imagens/outros/painelCompleto.jpg";
+  }
+}
+
+window.addEventListener('load', trocarImagem);
+window.addEventListener('resize', trocarImagem);
